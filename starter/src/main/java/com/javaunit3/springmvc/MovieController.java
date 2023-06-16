271d82db9d0d26bb1830d24a050a14276607e709
Katya Daron <katya_daron@hotmail.com>
2023-06-15 22:54:48 -0400

    Websites

---
+++ starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -0,0 +1,39 @@
+package com.javaunit3.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MovieController {
+
+    @RequestMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+
+    @RequestMapping("/bestMovie")
+    public String getBestMoviePage(Model model) {
+        Movie movie = new TitanicMovie();
+        BestMovieService bestMovieService = new BestMovieService(movie);
+        model.addAttribute("BestMovie", bestMovieService.getBestMovie().getTitle());
+        return "bestMovie";
+    }
+
+    @RequestMapping("/voteForBestMovieForm")
+    public String voteForBestMovieFormPage() {
+        return "voteForBestMovie";
+    }
+
+    @RequestMapping("/voteForBestMovie")
+    public String voteForBestMovie(HttpServletRequest request, Model model) {
+
+        String movieTitle = request.getParameter("movieTitle");
+
+        model.addAttribute("BestMovieVote", movieTitle);
+
+        return "voteForBestMovie";
+    }
+}
